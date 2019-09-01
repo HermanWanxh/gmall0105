adefd2978988d265ddcb06d1d54199a7afa7f1da
HermanWan <82267587@qq.com>
2019-09-01 23:41:54 +0800

    增加gmall-api,parent,common-util,web-util-service-util模块，将原gmall-user拆分

--- gmall-user/src/main/java/com/atguigu/gmall/user/service/impl/UserServiceImpl.java
+++ gmall-user/src/main/java/com/atguigu/gmall/user/service/impl/UserServiceImpl.java
@@ -1,8 +1,10 @@
 package com.atguigu.gmall.user.service.impl;
 
-import com.atguigu.gmall.user.bean.UmsMember;
+import com.atguigu.gmall.bean.UmsMember;
+import com.atguigu.gmall.bean.UmsMemberReceiveAddress;
+import com.atguigu.gmall.service.UserService;
+import com.atguigu.gmall.user.mapper.UmsMemberReceiveAddressMapper;
 import com.atguigu.gmall.user.mapper.UserMapper;
-import com.atguigu.gmall.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,12 +16,28 @@ public class UserServiceImpl implements UserService {
     @Autowired
     UserMapper userMapper;
 
+    @Autowired
+    UmsMemberReceiveAddressMapper umsMemberReceiveAddressMapper;
+
     @Override
     public List<UmsMember> getAllUser() {
 
         List<UmsMember> umsMemberList = userMapper.selectAll(); //userMapper.selectAllUser();
 
         return umsMemberList;
+    }
+
+    @Override
+    public List<UmsMemberReceiveAddress> getReceiveAddressByMemberId(String memberId) {
+        // 封装的参数对象
+        UmsMemberReceiveAddress umsMemberReceiveAddress = new UmsMemberReceiveAddress();
+        umsMemberReceiveAddress.setMemberId(memberId);
+        List<UmsMemberReceiveAddress> umsMemberReceiveAddressesList = umsMemberReceiveAddressMapper.select(umsMemberReceiveAddress);
+
+ //       Example example = new Example(UmsMemberReceiveAddress.class);
+ //       example.createCriteria().andEqualTo("memberId",memberId);
+ //       List<UmsMemberReceiveAddress> umsMemberReceiveAddressesList = umsMemberReceiveAddressMapper.selectByExample(example);
+        return umsMemberReceiveAddressesList;
     }
 }
 
